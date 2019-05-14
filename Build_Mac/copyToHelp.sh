43378fd95cd94977250cfde7b83f222b0610aed7
Daisuke Kamiyama <mi@mimikaki.net>
2019-05-15 00:34:26 +0900

    ヘルプ改善

--- Build_Mac/copyToHelp.sh
+++ Build_Mac/copyToHelp.sh
@@ -2,6 +2,9 @@
 mkdir -p ../help/ja/sdfsample
 mkdir -p ../help/ja/colorschemesample
 mkdir -p ../help/ja/csvsample
+mkdir -p ../help/ja/javascriptsample
 cp ../App/sdf/* ../help/ja/sdfsample/
 cp ../App/data/ColorScheme/* ../help/ja/colorschemesample
 cp ../default/HTML5/keyword/* ../help/ja/csvsample
+cp ../default/HTML5/plugins/syntaxchecker_jshint.js ../help/ja/javascriptsample
+iconv -f utf8 -t cp932 ../help/ja/javascriptsample/syntaxchecker_jshint.js > ../help/ja/javascriptsample/syntaxchecker_jshint_sjis.js
