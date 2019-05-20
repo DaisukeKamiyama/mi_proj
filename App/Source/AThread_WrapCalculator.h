a86c3a8b893693b043f552522915057e78cb11a9
Daisuke Kamiyama <mi@mimikaki.net>
2019-05-20 20:22:39 +0900

    #1421

--- App/Source/AThread_WrapCalculator.h
+++ App/Source/AThread_WrapCalculator.h
@@ -51,7 +51,7 @@ class AThread_WrapCalculator : public AThread
 			const AText& inFontName, const AFloatNumber inFontSize, const ABool inAntiAliasing,
 			const AItemCount inTabWidth, const AItemCount inIndentWidth, 
 			const AWrapMode inWrapMode, const AItemCount inWrapLetterCount, 
-			const ANumber inViewWidth, const ABool inCountAs2Letters );
+			const ANumber inViewWidth, const ABool inCountAs2Letters, const AArray<AIndex>& inTabPositions );//#1421
 	
 	//スレッド終了後処理
   public:
@@ -73,6 +73,7 @@ class AThread_WrapCalculator : public AThread
 	AItemCount					mWrapLetterCount;
 	ANumber						mViewWidth;
 	ABool						mCountAs2Letters;
+	AArray<AIndex>				mTabPositions;//#1421
 	
 };
 
